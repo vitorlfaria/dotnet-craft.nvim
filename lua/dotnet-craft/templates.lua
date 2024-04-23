e368f4b14510094532f89badea32af5af5686035
Vitor Lacerda <vitorlacerdafaria7@gmail.com>
2024-04-23 11:08:02 -0300

    feat: add new templates

--- lua/dotnet-craft/templates.lua
+++ lua/dotnet-craft/templates.lua
@@ -8,4 +8,73 @@ public class {{name}}
 }
 ]]
 
+M["Sealed Class"] = [[
+namespace {{namespace}};
+
+public sealed class {{name}}
+{
+}
+]]
+
+M["Static Class"] = [[
+namespace {{namespace}};
+
+public static class {{name}}
+{
+}
+]]
+
+M["Interface"] = [[
+namespace {{namespace}};
+
+public interface {{name}}
+{
+}
+]]
+
+M["Record"] = [[
+namespace {{namespace}};
+
+public record {{name}}
+{
+}
+]]
+
+M["Sealed Record"] = [[
+namespace {{namespace}};
+
+public sealed record {{name}}
+{
+}
+]]
+
+M["Enum"] = [[
+namespace {{namespace}};
+
+public enum {{name}}
+{
+}
+]]
+
+M["Struct"] = [[
+namespace {{namespace}};
+
+public struct {{name}}
+{
+}
+]]
+
+M["Api Controller"] = [[
+using Microsoft.AspNetCore.Mvc;
+
+namespace {{namespace}};
+
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+public class {{name}} : ControllerBase
+{
+}
+]]
+
 return M
